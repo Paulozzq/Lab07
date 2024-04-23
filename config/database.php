e8f8b5666c28fd6e90b899e1a1c6f0748801bee6
Linel121 <pipipixaty08@gmail.com>
2024-04-23 14:40:10 -0500

    cambioo

--- config/database.php
+++ config/database.php
@@ -20,38 +20,7 @@ class Database{
             echo 'Error de conexion :'.$e->getMessage();
             exit;
         }
-    }
-    function insertarUsuario($nombre, $apellido, $edad, $dni, $numero, $correo, $administrador, $nombre_usuario, $pass){
-        $stmt = $this->pdo->prepare("INSERT INTO usuario (nombre, apellido, edad, dni, numero, correo, administrador, nombre_usuario, pass) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->execute([$nombre, $apellido, $edad, $dni, $numero, $correo, $administrador, $nombre_usuario, $pass]);
-        return $stmt->rowCount();
-    }
-
-    // Read
-    function obtenerUsuarios(){
-        $stmt = $this->pdo->query("SELECT * FROM usuario");
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    function obtenerUsuarioPorId($id){
-        $stmt = $this->pdo->prepare("SELECT * FROM usuario  WHERE id = ?");
-        $stmt->execute([$id]);
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
-
-    // Update
-    function actualizarUsuario($id, $nombre, $apellido, $edad, $dni, $numero, $correo, $administrador, $nombre_usuario, $pass){
-        $stmt = $this->pdo->prepare("UPDATE usuario SET nombre = ?, apellido = ?, edad = ?, dni = ?, numero = ?, correo = ?, administrador = ?, nombre_usuario = ?, pass = ? WHERE id = ?");
-        $stmt->execute([$nombre, $apellido, $edad, $dni, $numero, $correo, $administrador, $nombre_usuario, $pass, $id]);
-        return $stmt->rowCount();
-    }
-
-    // Delete
-    function eliminarUsuario($id){
-        $stmt = $this->pdo->prepare("DELETE FROM usuario  WHERE id = ?");
-        $stmt->execute([$id]);
-        return $stmt->rowCount();
-    }
+    }   
 }
 
 
